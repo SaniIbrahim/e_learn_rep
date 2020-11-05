67bfd77c4554e2b3a04e1a1835b237e9f28a162e
SaniIbrahim <saniibrahimvws1@gmail.com>
2020-11-05 06:52:01 +0100

    New updates for Heroku deployment

--- educa/settings.py
+++ educa/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_xlrn^@75rkw!1dohl#=$rokvs&5xxpf)7(bx^_mq@9$4cst(@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
